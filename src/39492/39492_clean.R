54d02dbd9157bc78dd18b5c05ff3d215206404e9
mtwente <127845092+mtwente@users.noreply.github.com>
2025-04-22 18:34:37 +0200

    feat: add abb39492

---
+++ src/39492/39492_clean.R
@@ -0,0 +1,44 @@
+# Packages -------------------
+
+library(here)
+library(readxl)
+library(magrittr)
+library(csvwr)
+library(jsonlite)
+
+# Functions ------------------
+
+source(here("src", "Funktionen", "Metadata_JSON.R"))
+source(here("src", "Funktionen", "Export_CSV.R"))
+
+# Process Data --------
+
+data39492 <- read_excel(here("data", "raw", "Band3", "39492", "39492_Data_raw.xlsx"),
+                        sheet = 2,
+                        col_names = c("Startjahr", "Endjahr", "Zeitraum", "Summe Bürgeraufnahmen", "Summe Kriegsausgaben in Pfund"),
+                        range = "A2:E35")
+
+save_clean_csv(data39492, vol = 3)
+
+# Create Metadata -------
+
+meta39492 <- annotate(data = data39492,
+                      mediaID = 39492,
+                      vol = 3,
+                      title = "Bürgeraufnahmen und Kriegsausgaben, 1360–1530",
+                      column_description = c("Jahreszahl, Angabe in Jahren unserer Zeitrechnung", "Jahreszahl, Angabe in Jahren unserer Zeitrechnung", "Zeitraum, Angaben in Jahren unserer Zeitrechnung", "Summe der Bürgeraufnahmen im jeweiligen Zeitraum", "Summe der Kriegsausgaben im jeweiligen Zeitraum in Pfund"),
+                      subject = c("", ""),
+                      object_description = c("Der Zusammenhang zwischen Bürgeraufnahmen und Kriegszügen wird aus der Gegenüberstellung mit den städtischen Kriegsausgaben klar ersichtlich. Bis weit ins 15. Jahrhundert richtete sich die Einbürgerungspolitik nach den Bedürfnissen der Stadt im Kriegsfall und nicht nach der Migration. Entsprechend verging wohl für viele Zugezogene eine lange Zeit zwischen Zuwanderung und Einbürgerung (Daten nach: Portmann 1979, S. 96–99. Rosen 1984, S. 477–479)"),
+                      creator = list(list(name = "Benjamin Hitz")),
+                      contributor = list(list(name = "Nico Görlich",
+                                              orcid = "0000-0003-3860-1488"),
+                                         list(name = "Moritz Twente",
+                                              email = "moritz.twente@unibas.ch",
+                                              orcid = "0009-0005-7187-9774")),
+                      date = "1360/1525",
+                      temporal = "Neuzeit",
+                      source = "Portmann, Rolf Ernst: Basler Einbürgerungspolitik 1358–1798. Mit einer Berufs- und Herkunftsstatistik des Mittelalters, Basel 1979, S. 96–99. Rosen, Josef: Kriegsausgaben im Spätmittelalter. Der militärische Aufwand in Basel 1360–1535, in: Vierteljahrschrift für Sozial- und Wirtschaftsgeschichte 71, 1984, S. 477–479. Bearbeitung: Nico Görlich / Moritz Twente",
+                      relation = list("m39492_1", "m39492_2"),
+                      rights = "",
+                      license = ""
+)
---
+++ src/39492/39492_plot.R
@@ -0,0 +1,121 @@
+# Packages -------------------
+
+library(here)
+library(ggplot2)
+library(tidyr)
+library(scales)
+library(dplyr)
+library(patchwork)
+
+# Functions ------------------
+
+source(here("src", "Funktionen", "Format_Theme.R"))
+source(here("src", "Funktionen", "Format_Tausendertrennzeichen.R"))
+source(here("src", "Funktionen", "Format_Achsenbeschriftung.R"))
+source(here("src", "Funktionen", "Export_Plot.R"))
+
+# Read Data ------------------
+
+#data39492 <- readr::read_csv(here("Band3", "39492", "39492_Data.csv"))
+source(here("src", "39492", "39492_clean.R"))
+
+# Plots -----------------------
+
+# vertikale Linien zur besseren Ablesbarkeit
+vertical_lines <- seq(1375, 1525, by = 15)
+
+## erster Plot: Kriegsausgaben -----------
+
+plot39492a <- ggplot(data39492, aes(x = Startjahr)) +
+  
+  geom_vline(xintercept = vertical_lines,
+             color = "#777777", linewidth = 0.14,
+             linetype = "longdash") +
+  
+  geom_line(aes(y = `Summe Kriegsausgaben in Pfund`, colour = "Summe Kriegsausgaben in Pfund"),
+            linewidth = 0.561) +
+  
+  scale_colour_manual(values = c("Summe Kriegsausgaben in Pfund" = "#F7CB45"),
+                      labels = "Summe Kriegs-\nausgaben in Pfund",
+                      drop = FALSE) +
+  
+  scale_x_continuous(
+    breaks = seq(1360, 1525, by = 5),
+    labels = everysecond(data39492$Zeitraum),
+    guide = guide_axis(angle = 45),
+    expand = expansion(mult = c(0, 0))
+  ) +
+
+  scale_y_continuous(
+    limits = c(0, 40000),
+    breaks = pretty_breaks(),
+    expand = expansion(mult = c(0, 0)),
+    labels = ch_numbers
+  ) +
+  
+  coord_cartesian(clip = "off") +
+  
+  theme_sgb_basis() +
+  theme(legend.position = "none",
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.y = element_text(margin = margin(r = 5)),
+        plot.margin = margin(0.5,0.25,0,0.2, "lines"))
+
+## zweiter Plot: Bürgeraufnahmen -----------
+
+plot39492b <- ggplot(data39492, aes(x = Startjahr)) +
+  
+  geom_vline(xintercept = vertical_lines,
+             color = "#777777", linewidth = 0.14,
+             linetype = "longdash") +
+  
+  geom_line(aes(y = `Summe Bürgeraufnahmen`, colour = "Bürgeraufnahmen"),
+            linewidth = 0.561) +
+  
+  scale_colour_manual(values = c("Bürgeraufnahmen" = "#6195CF"),
+                      drop = FALSE) +
+  
+  scale_x_continuous(
+    breaks = seq(1360, 1525, by = 5),
+    labels = everysecond(data39492$Zeitraum),
+    guide = guide_axis(angle = 45),
+    expand = expansion(mult = c(0, 0)),
+  ) +
+
+  scale_y_continuous(
+    limits = c(0, 1200),
+    breaks = seq(0, 1200, by = 300),
+    expand = expansion(mult = c(0, 0)),
+    labels = ch_numbers
+  ) +
+  
+  coord_cartesian(clip = "off") +
+  
+  theme_sgb_basis() +
+  theme(legend.position = "none",
+        axis.text.y = element_text(margin = margin(r = 5)),
+        plot.margin = margin(0.5,0.25,0,0.2, "lines"))
+
+# Combine Plots ---------
+
+## Plot 39492a: get legend
+plot39492a_with_legend <- plot39492a + theme(legend.position = "right",
+                                             legend.justification = c("left", "bottom"))
+
+legende39492a <- get_legend(plot39492a_with_legend) %>%
+  as_ggplot()
+
+## Plot 39492b: get legend
+plot39492b_with_legend <- plot39492b + theme(legend.position = "right",
+                                             legend.justification = c("left", "bottom"))
+
+legende39492b <- get_legend(plot39492b_with_legend) %>%
+  as_ggplot()
+
+## Combine Plots with patchwork
+plot39492 <- plot39492a + legende39492a + plot39492b + legende39492b + plot_layout(ncol = 2, widths = c(4, 1))
+
+# Export -----------------------
+
+export_plot(plot39492, 3, 175, 139)
