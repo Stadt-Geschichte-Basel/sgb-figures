81a425d578697902785ae6ca5aa464fdae92f872
mtwente <127845092+mtwente@users.noreply.github.com>
2025-06-19 11:24:03 +0200

    feat: add plot 39543

---
+++ src/39543/39543_clean.R
@@ -0,0 +1,56 @@
+# Packages -------------------
+
+library(here)
+library(readxl)
+library(magrittr)
+library(csvwr)
+library(jsonlite)
+
+# Functions ------------------
+
+source(here("src", "Funktionen", "Metadata_JSON.R"))
+source(here("src", "Funktionen", "Export_CSV.R"))
+
+# Process Data --------
+
+data39543 <- read_excel(here("data", "raw", "Band3", "39543", "39543_Data_raw.xlsx"),
+                        col_names = c("Startjahr", "Endjahr", "Zeitraum", "Wundtat", "Totschlag", "«Geloiff» (Tumult)", "Ehrverletzung", "Jugenddelikt", "Ungehorsam", "Eigentum", "Sexualdelikt", "Meineid", "Gotteslästerung"),
+                        range = "A2:M9")
+
+col_order <- c("Startjahr", "Endjahr", "Zeitraum",
+               "Gotteslästerung",
+               "Ehrverletzung",
+               "Meineid",
+               "Ungehorsam",
+               "Jugenddelikt",
+               "Eigentum",
+               "Sexualdelikt",
+               "«Geloiff» (Tumult)",
+               "Totschlag",
+               "Wundtat")
+
+data39543 <- data39543[,col_order]
+
+save_clean_csv(data39543, vol = 3)
+
+# Create Metadata -------
+
+meta39543 <- annotate(data = data39543,
+                      mediaID = 39543,
+                      vol = 3,
+                      title = "Verbannungsurteile, 1376–1455",
+                      column_description = c("Jahreszahl, Angabe in Jahren unserer Zeitrechnung", "Jahreszahl, Angabe in Jahren unserer Zeitrechnung", "Zeitraum, Angabe von Start- und Endjahr als Jahreszahl unserer Zeitrechnung", "Anzahl der Verbannungsurteile wegen Gotteslästerung im angegebenen Zeitraum", "Anzahl der Verbannungsurteile wegen Ehrverletzung im angegebenen Zeitraum", "Anzahl der Verbannungsurteile wegen Meineid im angegebenen Zeitraum", "Anzahl der Verbannungsurteile wegen Ungehorsam im angegebenen Zeitraum", "Anzahl der Verbannungsurteile wegen Jugenddelikten im angegebenen Zeitraum", "Anzahl der Verbannungsurteile wegen Eigentumsdelikten im angegebenen Zeitraum", "Anzahl der Verbannungsurteile wegen Sexualdelikten im angegebenen Zeitraum", "Anzahl der Verbannungsurteile wegen Geloiff (Tumult) im angegebenen Zeitraum", "Anzahl der Verbannungsurteile wegen Totschlag im angegebenen Zeitraum", "Anzahl der Verbannungsurteile wegen Wundtaten im angegebenen Zeitraum"),
+                      object_description = c("Die weitaus umfangreichste Urteilskategorie bildeten über den ganzen Zeitraum die Wundtaten, auch Totschläge waren häufig. Demzufolge waren mehr als die Hälfte aller Verbannungen auf Gewalttaten zurückzuführen, obwohl diese im Gegensatz etwa zu Eigentumsdelikten nicht ehrrührig waren (Hagemann 1981, Bd. 1, S. 155f.; Wackernagel 1907-1924, Bd. 2.1, S. 345; Rippmann; Simon-Murscheid 1991, S. 24). Der Frauenanteil unter den Verbannten stieg im Verlauf der untersuchten Zeit tendenziell an, wobei die Täterinnen von Sexualdelikten in der zweiten Hälfte die Täterinnen von Eigentumsdelikten als die grösste Gruppe ablösten (Datengrundlage: Simon-Muscheid 1991, S. 30f.)"),
+                      creator = list(list(name = "Benjamin Hitz",
+                                          orcid = "0000-0002-3208-4881")),
+                      contributor = list(list(name = "Nico Görlich",
+                                              orcid = "0000-0003-3860-1488"),
+                                         list(name = "Moritz Twente",
+                                              email = "moritz.twente@unibas.ch",
+                                              orcid = "0009-0005-7187-9774")),
+                      date = "1376/1455",
+                      temporal = "Frühe Neuzeit",
+                      source = "Simon-Muscheid, Katharina: Gewalt und Ehre im spätmittelalterlichen Handwerk am Beispiel Basels, in: Zeitschrift für historische Forschung 18, 1991, S. 1–31, hier S. 30 f. Bearbeitung: Nico Görlich / Moritz Twente",
+                      rights = "CC BY-SA, Quelle: Simon-Muscheid 1977. Bearbeitung: Nico Görlich / Moritz Twente",
+                      relation = list("m39543_1", "m39543_2")
+)
---
+++ src/39543/39543_plot.R
@@ -0,0 +1,88 @@
+# Packages -------------------
+
+library(here)
+library(ggplot2)
+library(tidyr)
+library(scales)
+library(dplyr)
+library(zoo)
+
+# Functions ------------------
+
+source(here("src", "Funktionen", "Format_Theme.R"))
+source(here("src", "Funktionen", "Format_Tausendertrennzeichen.R"))
+source(here("src", "Funktionen", "Format_Achsenbeschriftung.R"))
+source(here("src", "Funktionen", "Export_Plot.R"))
+
+# Read Data ------------------
+
+#data39543 <- readr::read_csv(here("Band3", "39543", "39543_Data.csv"))
+source(here("src", "39543", "39543_clean.R"))
+
+# Transform Data ------------------
+
+data39543_longer <- data39543[,-2:-3]
+
+data39543_longer <- data39543_longer %>%
+  pivot_longer(
+    !Startjahr,
+    names_to = "Urteilstyp",
+    values_to = "Anzahl"
+  )
+
+data39543_longer$Urteilstyp <- factor(data39543_longer$Urteilstyp, 
+                                      levels = c("Gotteslästerung", 
+                                                 "Ehrverletzung",
+                                                 "Meineid",
+                                                 "Ungehorsam",
+                                                 "Jugenddelikt",
+                                                 "Eigentum",
+                                                 "Sexualdelikt",
+                                                 "«Geloiff» (Tumult)",
+                                                 "Totschlag",
+                                                 "Wundtat"))
+
+# Plot -----------------------
+
+plot39543 <- ggplot(data39543_longer,
+                    aes(x = Startjahr, y = Anzahl, fill = Urteilstyp)) +
+  
+  geom_bar(stat = "identity", position = "fill") +
+
+    scale_fill_manual(values = c("Gotteslästerung" = "#777777",
+                                "Ehrverletzung" = "#90C987",
+                                "Meineid" =  "#FAE7B5",
+                                "Ungehorsam" = "#F7CB45",
+                                "Jugenddelikt" = "#A5170E",
+                                "Eigentum" = "#E8601C",
+                                "Sexualdelikt" = "#994F88",
+                                "«Geloiff» (Tumult)" = "#4EB265",
+                                "Totschlag" = "#b3c1d4", 
+                                "Wundtat" = "#7BAFDE")) +
+  
+  scale_x_continuous(
+    breaks = data39543$Startjahr,
+    labels = data39543$Zeitraum,
+    guide = guide_axis(angle = 0),
+    expand = expansion(mult = c(0, 0))
+  ) +
+
+  scale_y_continuous(
+    breaks = breaks_pretty(),
+    labels = label_percent(),
+    expand = expansion(mult = c(0, 0))
+  ) +
+  
+  coord_cartesian(clip = "off") +
+  
+  theme_sgb_basis() +
+  theme(legend.position = "none",
+        axis.ticks.x = element_blank(),
+        axis.text.y = element_text(margin = margin(r = 5)),
+        legend.key.height = unit(2.05, "mm"), # entspricht 1.64mm
+        legend.key.width = unit(5, "mm"), # entspricht 4mm
+        plot.margin = margin(0.5,0.2,0,0, "lines"))
+
+# Export -----------------------
+
+export_plot(plot39543, 3, 129.8, 72.2, 30, 26)
