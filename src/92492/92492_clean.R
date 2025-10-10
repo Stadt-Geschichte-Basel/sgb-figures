6337fc7a62163ce05f1b2f73dafed3c5fde0af69
mtwente <127845092+mtwente@users.noreply.github.com>
2025-10-10 16:36:28 +0200

    feat: add new metadata sample for 92492

--- src/92492/92492_clean.R
+++ src/92492/92492_clean.R
@@ -21,7 +21,7 @@ save_clean_csv(data92492, csv_suffix = 3, vol = 6)
 
 # Create Metadata ------------
 
-meta92492 <- annotate(
+meta92492 <- annotate2(
   data = data92492,
   media_id = 92492,
   csv_suffix = 3,
